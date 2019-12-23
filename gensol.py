8f48e66986e9c784684357e6f1e7eb6d16b0a27b
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-12-23 19:06:29 +0100

    Don't use I/O to evaluate whether a solution is feasible

--- gensol.py
+++ gensol.py
@@ -1,4 +1,5 @@
 import sqlite3
+import logging
 import random
 import csv
 import scorer
@@ -7,7 +8,6 @@ LOAN_FILE = './data/LoanData.csv'
 OPTION_FILE = './data/PoolOptionData.csv'
 COMBO_FILE = './data/EligiblePricingCombinations.csv'
 CONSTRAINT_FILE = './data/ConstraintA.csv'
-SOLUTION_FILE = './solution/A.csv'
 
 def choose_buyers():
     chosen_buyer = {}
@@ -19,6 +19,7 @@ def choose_buyers():
 
 SEED = 40
 random.seed(SEED)
+logging.error(f'Random seed set to {SEED}')
 
 # get the dataset from the database
 connection = sqlite3.connect('data/mortgages.db')
@@ -39,41 +40,30 @@ for selling in sellings:
     loan_buyers.append((pool, servicer))
 
 loans, pools, combs, constraints, states, \
-occupancy_types, purposes, property_types = load_everything(LOAN_FILE, OPTION_FILE, COMBO_FILE, CONSTRAINT_FILE)
-
-
+occupancy_types, purposes, property_types = scorer.load_everything(LOAN_FILE, OPTION_FILE, COMBO_FILE, CONSTRAINT_FILE)
 
 feasible = False
-i = 0
+solution_count = 0
 while not feasible:
     chosen_buyer = choose_buyers()
-    i += 1
-    print(f'Chosen solution {i} of random seed {SEED}')
+    solution_count += 1
+    logging.error(f'Solution #{solution_count}')
 
-    with open(SOLUTION_FILE, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
+    feasible = True
+    try:
         obj = 0
         used = set()
-        for row in reader:
-            i = row['Loan']
-            j = row['Pool']
-            k = row['Servicer']
+        for i, (j, k) in chosen_buyer.items():
             assert (i, j, k) in combs, f'Invalid combinations! {i}, {j}, {k}'
             assert (i, j, k) not in used, 'Duplicated combinations!'
             used.add((i, j, k))
             pijk = combs[(i, j, k)]
             obj += float(pijk) / 100 * loans[i].Li
-    return obj, used
 
-    with open('solution/A.csv', 'w') as solution_file:
-        writer = csv.writer(solution_file)
-        writer.writerow(['Loan','Pool','Servicer'])
-        for loan_id, (pool, servicer) in chosen_buyer.items():
-            writer.writerow([loan_id, pool, servicer])
-
-    feasible = True
-    try:
-        score = evaluate_loaded(loans, pools, combs, constraints, states, \
+        score = scorer.evaluate_loaded(loans, pools, combs, constraints, states, \
         occupancy_types, purposes, property_types, obj, used)
     except AssertionError as e:
         feasible = False
+
+print('Found feasible solution:')
+print(chosen_buyer)
