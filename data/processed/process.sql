380474dbe6025476774b601cc3bfee42b1d34dc6
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-12-27 20:00:31 +0100

    Extract simplified CSV files from the given ones

---
+++ data/processed/process.sql
@@ -0,0 +1,25 @@
+
+SELECT LoanID AS Loan,
+	Amount, FICO AS Fico,
+	DTI AS Dti,
+	HighBalFlag == 'Y' AS IsExpensive,
+	PropState == 'CA' AS IsCalifornia,
+	Purpose == 'Cashout' AS IsCashout,
+	PropOcc == 'Primary' AS IsPrimary FROM LoanData;
+
+SELECT PoolOptionj AS Pool,
+	PoolType,
+	PoolBalanceType,
+	Servicer FROM PoolOptionData;
+
+SELECT EligiblePricingCombinations.LoanID AS Loan,
+	PoolOptonj AS Pool,
+	PriceP_ijk / 100 * Amount AS Price
+	FROM EligiblePricingCombinations INNER JOIN LoanData
+	ON EligiblePricingCombinations.LoanID == LoanData.LoanID ORDER BY LoanData.LoanID;
+
+SELECT PoolOptonj AS Pool,
+	LoanData.LoanID AS Loan,
+	PriceP_ijk / 100 * Amount AS Price
+	FROM EligiblePricingCombinations INNER JOIN LoanData
+	ON EligiblePricingCombinations.LoanID == LoanData.LoanID ORDER BY Pool;
