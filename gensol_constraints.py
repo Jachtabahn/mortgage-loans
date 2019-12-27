6193d57d4f3b7110ccd7248b91edb08bbd542ec6
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-12-27 21:31:19 +0100

    Extract all 1-subsets of buyers who fulfill all their conditions

--- gensol_constraints.py
+++ gensol_constraints.py
@@ -3,6 +3,29 @@ import itertools
 import logging
 import csv
 
+def compute_feasible_subsets(pool, pool_buyers):
+    feasible_subsets = []
+    num_subsets, num_feasible = 0, 0
+    for subset_buyers in itertools.combinations(pool_buyers, 1):
+        num_subsets += 1
+        logging.debug(f'Subset of buyers {num_subsets}')
+        for buyer in subset_buyers:
+            logging.debug(buyer)
+
+        # check constraints on the subset_buyers given the loans and pools
+        subset_loan_ids = [buyer.loan_id for buyer in subset_buyers]
+        subset_loans = [loans[loan_id] for loan_id in subset_loan_ids]
+        show_list(subset_loans)
+
+        ok = check_constraints(pool, subset_loans)
+        logging.debug(f'OK: {ok}')
+        logging.debug(f'---------------------------------------------------\n\n')
+
+        if ok:
+            num_feasible += 1
+            feasible_subsets.append(subset_buyers)
+    return feasible_subsets
+
 def check_constraints(pool, subset_loans):
     subset_size = len(subset_loans)
 
@@ -28,94 +51,120 @@ def check_constraints(pool, subset_loans):
     averaged_primaries = [loan.is_primary / subset_size for loan in subset_loans]
     average_primary = sum(averaged_primaries)
 
-    logging.info(f'Sum of amounts: {amounts_sum}\n')
+    logging.debug(f'Sum of amounts: {amounts_sum}\n')
 
     show_list(weights, 'Weights:')
 
     show_list(normalized_ficos, 'Normalized FICOs:')
-    logging.info(f'Normal FICO: {normal_fico}\n')
+    logging.debug(f'Normal FICO: {normal_fico}\n')
 
     show_list(normalized_dtis, 'Normalized DTIs:')
-    logging.info(f'Normal DTI: {normal_dti}\n')
+    logging.debug(f'Normal DTI: {normal_dti}\n')
 
     show_list(normalized_expenses, 'Normalized expenses:')
-    logging.info(f'Normal expense: {normal_expense}\n')
+    logging.debug(f'Normal expense: {normal_expense}\n')
 
     show_list(averaged_californias, 'Averaged californias:')
-    logging.info(f'Average california: {average_california}\n')
+    logging.debug(f'Average california: {average_california}\n')
 
     show_list(averaged_cashouts, 'Averaged cashouts:')
-    logging.info(f'Average cashout: {average_cashout}\n')
+    logging.debug(f'Average cashout: {average_cashout}\n')
 
     show_list(averaged_primaries, 'Averaged primaries:')
-    logging.info(f'Average primary: {average_primary}\n')
+    logging.debug(f'Average primary: {average_primary}\n')
+
+    ok = True
 
     if pool.is_standard:
-        logging.info('Check c1: normal_expense <= c1')
-        logging.info(f'Check c1: {normal_expense} <= {constraints["c1"]}')
-        logging.info(f'Check c1: {normal_expense <= constraints["c1"]}')
+        logging.debug('Check c1: normal_expense <= c1')
+        logging.debug(f'Check c1: {normal_expense} <= {constraints["c1"]}')
+        c1 = (normal_expense <= constraints["c1"])
+        logging.debug(f'Check c1: {c1}')
+        ok = ok and c1
 
     if pool.is_single:
-        logging.info('Check c2: amounts_sum >= c2')
-        logging.info(f'Check c2: {amounts_sum} >= {constraints["c2"]}')
-        logging.info(f'Check c2: {amounts_sum >= constraints["c2"]}')
+        logging.debug('Check c2: amounts_sum >= c2')
+        logging.debug(f'Check c2: {amounts_sum} >= {constraints["c2"]}')
+        c2 = (amounts_sum >= constraints["c2"])
+        logging.debug(f'Check c2: {c2}')
+        ok = ok and c2
 
     if pool.servicer == 'Pingora':
-        logging.info('Check c3: amounts_sum <= c3')
-        logging.info(f'Check c3: {amounts_sum} <= {constraints["c3"]}')
-        logging.info(f'Check c3: {amounts_sum <= constraints["c3"]}')
+        logging.debug('Check c3: amounts_sum <= c3')
+        logging.debug(f'Check c3: {amounts_sum} <= {constraints["c3"]}')
+        c3 = (amounts_sum <= constraints["c3"])
+        logging.debug(f'Check c3: {c3}')
+        ok = ok and c3
 
-        logging.info('Check c4: normal_expense <= c4')
-        logging.info(f'Check c4: {normal_expense} <= {constraints["c4"]}')
-        logging.info(f'Check c4: {normal_expense <= constraints["c4"]}')
+        logging.debug('Check c4: normal_expense <= c4')
+        logging.debug(f'Check c4: {normal_expense} <= {constraints["c4"]}')
+        c4 = (normal_expense <= constraints["c4"])
+        logging.debug(f'Check c4: {c4}')
+        ok = ok and c4
 
-        logging.info('Check c5: normal_fico >= c5')
-        logging.info(f'Check c5: {normal_fico} >= {constraints["c5"]}')
-        logging.info(f'Check c5: {normal_fico >= constraints["c5"]}')
+        logging.debug('Check c5: normal_fico >= c5')
+        logging.debug(f'Check c5: {normal_fico} >= {constraints["c5"]}')
+        c5 = (normal_fico >= constraints["c5"])
+        logging.debug(f'Check c5: {c5}')
+        ok = ok and c5
 
-        logging.info('Check c6: normal_dti <= c6')
-        logging.info(f'Check c6: {normal_dti} <= {constraints["c6"]}')
-        logging.info(f'Check c6: {normal_dti <= constraints["c6"]}')
+        logging.debug('Check c6: normal_dti <= c6')
+        logging.debug(f'Check c6: {normal_dti} <= {constraints["c6"]}')
+        c6 = (normal_dti <= constraints["c6"])
+        logging.debug(f'Check c6: {c6}')
+        ok = ok and c6
 
-        logging.info('Check c7: average_california <= c7')
-        logging.info(f'Check c7: {average_california} <= {constraints["c7"]}')
-        logging.info(f'Check c7: {average_california <= constraints["c7"]}')
+        logging.debug('Check c7: average_california <= c7')
+        logging.debug(f'Check c7: {average_california} <= {constraints["c7"]}')
+        c7 = (average_california <= constraints["c7"])
+        logging.debug(f'Check c7: {c7}')
+        ok = ok and c7
 
     if pool.servicer == 'Two Harbors':
-        logging.info('Check c8: amounts_sum >= c8')
-        logging.info(f'Check c8: {amounts_sum} >= {constraints["c8"]}')
-        logging.info(f'Check c8: {amounts_sum >= constraints["c8"]}')
+        logging.debug('Check c8: amounts_sum >= c8')
+        logging.debug(f'Check c8: {amounts_sum} >= {constraints["c8"]}')
+        c8 = (amounts_sum >= constraints["c8"])
+        logging.debug(f'Check c8: {c8}')
+        ok = ok and c8
 
-        logging.info('Check c9: normal_fico >= c9')
-        logging.info(f'Check c9: {normal_fico} >= {constraints["c9"]}')
-        logging.info(f'Check c9: {normal_fico >= constraints["c9"]}')
+        logging.debug('Check c9: normal_fico >= c9')
+        logging.debug(f'Check c9: {normal_fico} >= {constraints["c9"]}')
+        c9 = (normal_fico >= constraints["c9"])
+        logging.debug(f'Check c9: {c9}')
+        ok = ok and c9
 
-        logging.info('Check c10: normal_dti <= c10')
-        logging.info(f'Check c10: {normal_dti} <= {constraints["c10"]}')
-        logging.info(f'Check c10: {normal_dti <= constraints["c10"]}')
+        logging.debug('Check c10: normal_dti <= c10')
+        logging.debug(f'Check c10: {normal_dti} <= {constraints["c10"]}')
+        c10 = (normal_dti <= constraints["c10"])
+        logging.debug(f'Check c10: {c10}')
+        ok = ok and c10
 
-        logging.info('Check c11: average_cashout <= c11')
-        logging.info(f'Check c11: {average_cashout} <= {constraints["c11"]}')
-        logging.info(f'Check c11: {average_cashout <= constraints["c11"]}')
+        logging.debug('Check c11: average_cashout <= c11')
+        logging.debug(f'Check c11: {average_cashout} <= {constraints["c11"]}')
+        c11 = (average_cashout <= constraints["c11"])
+        logging.debug(f'Check c11: {c11}')
+        ok = ok and c11
 
-        logging.info('Check c12: average_primary >= c12')
-        logging.info(f'Check c12: {average_primary} >= {constraints["c12"]}')
-        logging.info(f'Check c12: {average_primary >= constraints["c12"]}')
+        logging.debug('Check c12: average_primary >= c12')
+        logging.debug(f'Check c12: {average_primary} >= {constraints["c12"]}')
+        c12 = (average_primary >= constraints["c12"])
+        logging.debug(f'Check c12: {c12}')
+        ok = ok and c12
 
-
+    return ok
 
 def show_dict(items):
     for _, item in items.items():
-        logging.info(item)
-        logging.info('')
+        logging.debug(item)
+        logging.debug('')
 
 def show_list(items, title=None):
     if title is not None:
-        logging.info(title)
+        logging.debug(title)
 
     for item in items:
-        logging.info(item)
-        logging.info('')
+        logging.debug(item)
+        logging.debug('')
 
 def show_all(loans, pools, constraints, buyers):
     show_dict(loans)
@@ -175,8 +224,8 @@ class Pool:
 
 class Buyer:
 
-    def __init__(self, pool_id, loan_id, price):
-        self.id = (pool_id, loan_id)
+    def __init__(self, buyer_id, pool_id, loan_id, price):
+        self.id = buyer_id
         self.pool_id = pool_id
         self.loan_id = loan_id
         self.price = price
@@ -263,10 +312,10 @@ if __name__ == '__main__':
         next(pool_loans_reader) # consume the column names
         for pool_id_string, loan_id_string, price_string in pool_loans_reader:
             pool_id = int(pool_id_string[5:])
-            buyer = Buyer(pool_id, int(loan_id_string), float(price_string))
+            buyer = Buyer(len(buyers), pool_id, int(loan_id_string), float(price_string))
             buyers.append(buyer)
 
-            assert type(buyer.id) == tuple
+            assert type(buyer.id) == int
             assert type(buyer.price) == float
             assert type(buyer.pool_id) == int
             assert type(buyer.loan_id) == int
@@ -276,27 +325,53 @@ if __name__ == '__main__':
     assert type(constraints) == dict, 'Constraints should be in a dict'
     assert type(buyers) == list, 'Buyers should be in a list'
 
-    pool_id = 1
-    pool = pools[pool_id]
-    logging.info(pool)
+    buyer_subsets = []
 
-    subset_loan_ids = [600144439, 600144677]
-    subset_loans = [loans[loan_id] for loan_id in subset_loan_ids]
-    show_list(subset_loans)
+    num_total_subsets = 0
+    logging.debug(f'Will only consider subsets of size 1..')
 
-    check_constraints(pool, subset_loans)
+    for pool_id in pools:
+        pool = pools[pool_id]
+        pool_buyers = [buyer for buyer in buyers if buyer.pool_id == pool_id]
+        feasible_subsets = compute_feasible_subsets(pool, pool_buyers)
+        buyer_subsets.extend(feasible_subsets)
 
-    # special_buyers = [buyer for buyer in buyers if buyer.pool_id == 1]
-    # num_special_buyers = len(special_buyers)
-    # logging.info(f'num_special_buyers: {num_special_buyers}')
+        # num_total_subsets += num_pool_subsets
+        # logging.info(f'Found {num_feasible} feasible subsets out of {num_pool_subsets} subsets for pool {pool_id}')
+        # logging.info(f'Found {len(buyer_subsets)} feasible subsets out of {num_total_subsets} considered subsets in total.')
+        # logging.info('-------------------------------------------------------\n')
 
-    # for i, subset in enumerate(itertools.combinations(special_buyers, 2)):
-    #     logging.info(f'Subset {i}')
+    # for subset in buyer_subsets:
+    #     logging.info('--------------->')
     #     for buyer in subset:
     #         logging.info(buyer)
+    #     logging.info('<---------------')
 
-    #     # check constraints on the subset given the loans and pools
-    #     subset_loan_ids = [buyer.loan_id for buyer in subset]
-    #     subset_loans = [loans[loan_id] for loan_id in subset_loan_ids]
-    #     show_dict(subset_loans)
-    #     loans_sum = sum()
+    logging.info(f'We know {len(buyer_subsets)} feasible subsets.')
+
+    chosen_buyers = []
+
+    chosen_subset = buyer_subsets[0]
+    chosen_buyers.extend(chosen_subset)
+
+    chosen_loan_ids = [buyer.loan_id for buyer in chosen_subset]
+
+    logging.info(f'Chosen loan ids: {chosen_loan_ids}')
+
+    delete_indices = []
+    for i, subset in enumerate(buyer_subsets):
+        contains_chosen_loan = False
+        for buyer in subset:
+            if buyer.loan_id in chosen_loan_ids:
+                contains_chosen_loan = True
+                break
+        if contains_chosen_loan:
+            delete_indices.append(i)
+
+    for i in delete_indices[::-1]:
+        subset = buyer_subsets[i]
+        for buyer in subset:
+            logging.info(buyer)
+        del buyer_subsets[i]
+
+    logging.info(f'We know {len(buyer_subsets)} feasible subsets.')
