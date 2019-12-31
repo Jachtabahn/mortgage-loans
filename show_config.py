a900a0437fc2ffac363fbb805cb9c2f47caa1044
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-12-31 15:33:44 +0100

    Construct the initial configuration vectors and transition to another one via a buyer

--- show_config.py
+++ show_config.py
@@ -160,130 +160,174 @@ class Buyer:
     def __str__(self):
         return buyer_string.format(self.id, self.pool_id, self.loan_id, self.price)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    # parser.add_argument('--network-name', '-g', default=None)
-    parser.add_argument('--verbose', '-v', action='count')
-    args = parser.parse_args()
+# MAIN PROGRAM START
+#######################################################################
 
-    log_levels = {
-        None: logging.WARNING,
-        1: logging.INFO,
-        2: logging.DEBUG
-    }
-    if args.verbose is not None and args.verbose >= len(log_levels):
-        args.verbose = len(log_levels)-1
-    logging.basicConfig(format='%(message)s', level=log_levels[args.verbose])
+parser = argparse.ArgumentParser()
+# parser.add_argument('--network-name', '-g', default=None)
+parser.add_argument('--verbose', '-v', action='count')
+args = parser.parse_args()
 
-    loans = {}
-    with open('processed_small/Loans.csv') as loans_file:
-        loans_reader = csv.reader(loans_file)
-        next(loans_reader) # consume the column names
-        for loan_id_string, amount_string, fico_string, dti_string,\
-                is_expensive_string, is_california_string,\
-                is_cashout_string, is_primary_string in loans_reader:
+log_levels = {
+    None: logging.WARNING,
+    1: logging.INFO,
+    2: logging.DEBUG
+}
+if args.verbose is not None and args.verbose >= len(log_levels):
+    args.verbose = len(log_levels)-1
+logging.basicConfig(format='%(message)s', level=log_levels[args.verbose])
 
-            # parse the loan row
-            loan_id = int(loan_id_string)
-            amount = float(amount_string)
-            fico = float(fico_string)
-            dti = float(dti_string)
-            is_expensive = int(is_expensive_string == '1')
-            is_california = int(is_california_string == '1')
-            is_cashout = int(is_cashout_string == '1')
-            is_primary = int(is_primary_string == '1')
+loans = {}
+with open('processed_small/Loans.csv') as loans_file:
+    loans_reader = csv.reader(loans_file)
+    next(loans_reader) # consume the column names
+    for loan_id_string, amount_string, fico_string, dti_string,\
+            is_expensive_string, is_california_string,\
+            is_cashout_string, is_primary_string in loans_reader:
 
-            loan = Loan(loan_id, amount, fico, dti,
-                is_expensive, is_california, is_cashout, is_primary)
-            loans[loan_id] = loan
+        # parse the loan row
+        loan_id = int(loan_id_string)
+        amount = float(amount_string)
+        fico = float(fico_string)
+        dti = float(dti_string)
+        is_expensive = int(is_expensive_string == '1')
+        is_california = int(is_california_string == '1')
+        is_cashout = int(is_cashout_string == '1')
+        is_primary = int(is_primary_string == '1')
 
-            assert type(loan.id) == int
-            assert type(loan.amount) == float
-            assert type(loan.fico) == float
-            assert type(loan.dti) == float
-            assert type(loan.is_expensive) == int, 'is_expensive should be an integer for easy multiplication and addition'
-            assert type(loan.is_california) == int, 'is_california should be an integer for easy multiplication and addition'
-            assert type(loan.is_cashout) == int, 'is_cashout should be an integer for easy multiplication and addition'
-            assert type(loan.is_primary) == int, 'is_primary should be an integer for easy multiplication and addition'
+        loan = Loan(loan_id, amount, fico, dti,
+            is_expensive, is_california, is_cashout, is_primary)
+        loans[loan_id] = loan
 
-    pools = {}
-    with open('processed_small/Pools.csv') as pools_file:
-        pools_reader = csv.reader(pools_file)
-        next(pools_reader) # consume the column names
-        for pool_id_string, standard_string, single_string, servicer in pools_reader:
+        assert type(loan.id) == int
+        assert type(loan.amount) == float
+        assert type(loan.fico) == float
+        assert type(loan.dti) == float
+        assert type(loan.is_expensive) == int, 'is_expensive should be an integer for easy multiplication and addition'
+        assert type(loan.is_california) == int, 'is_california should be an integer for easy multiplication and addition'
+        assert type(loan.is_cashout) == int, 'is_cashout should be an integer for easy multiplication and addition'
+        assert type(loan.is_primary) == int, 'is_primary should be an integer for easy multiplication and addition'
 
-            # parse the pool row
-            pool_id = int(pool_id_string[5:])
-            is_standard = (standard_string == 'Standard Balance')
-            is_single = (single_string == 'Single-Issuer')
+pools = {}
+with open('processed_small/Pools.csv') as pools_file:
+    pools_reader = csv.reader(pools_file)
+    next(pools_reader) # consume the column names
+    for pool_id_string, standard_string, single_string, servicer in pools_reader:
 
-            pool = Pool(pool_id, is_standard, is_single, servicer)
-            pools[pool_id] = pool
+        # parse the pool row
+        pool_id = int(pool_id_string[5:])
+        is_standard = (standard_string == 'Standard Balance')
+        is_single = (single_string == 'Single-Issuer')
 
-            assert type(pool.id) == int
-            assert type(pool.is_standard) == bool
-            assert type(pool.is_single) == bool
-            assert type(pool.servicer) == str
+        pool = Pool(pool_id, is_standard, is_single, servicer)
+        pools[pool_id] = pool
 
-    constraints = {}
-    with open('processed_small/Constraints.csv') as constraints_file:
-        constraints_reader = csv.reader(constraints_file)
-        for name, value_string in constraints_reader:
-            value = float(value_string)
-            constraints[name] = value
+        assert type(pool.id) == int
+        assert type(pool.is_standard) == bool
+        assert type(pool.is_single) == bool
+        assert type(pool.servicer) == str
 
-    buyers = []
-    with open('processed_small/ChooseLoan.csv') as choose_pool_file:
-        pool_loans_reader = csv.reader(choose_pool_file)
-        next(pool_loans_reader) # consume the column names
-        for pool_id_string, loan_id_string, price_string in pool_loans_reader:
-            pool_id = int(pool_id_string[5:])
-            buyer = Buyer(len(buyers), pool_id, int(loan_id_string), float(price_string))
-            buyers.append(buyer)
+constraints = {}
+with open('processed_small/Constraints.csv') as constraints_file:
+    constraints_reader = csv.reader(constraints_file)
+    for name, value_string in constraints_reader:
+        value = float(value_string)
+        constraints[name] = value
 
-            assert type(buyer.id) == int
-            assert type(buyer.price) == float
-            assert type(buyer.pool_id) == int
-            assert type(buyer.loan_id) == int
+buyers = []
+with open('processed_small/ChooseLoan.csv') as choose_pool_file:
+    pool_loans_reader = csv.reader(choose_pool_file)
+    next(pool_loans_reader) # consume the column names
+    for pool_id_string, loan_id_string, price_string in pool_loans_reader:
+        pool_id = int(pool_id_string[5:])
+        buyer = Buyer(len(buyers), pool_id, int(loan_id_string), float(price_string))
+        buyers.append(buyer)
 
-    assert type(loans) == dict, 'Loans should be in a dict'
-    assert type(pools) == dict, 'Pools should be in a dict'
-    assert type(constraints) == dict, 'Constraints should be in a dict'
-    assert type(buyers) == list, 'Buyers should be in a list'
+        assert type(buyer.id) == int
+        assert type(buyer.price) == float
+        assert type(buyer.pool_id) == int
+        assert type(buyer.loan_id) == int
 
-    # remove unnecessary loans (may be needed if this is a reduced input)
-    used_loan_ids = set([buyer.loan_id for buyer in buyers])
-    for loan_id in list(loans):
-        if loan_id not in used_loan_ids:
-            del loans[loan_id]
+assert type(loans) == dict, 'Loans should be in a dict'
+assert type(pools) == dict, 'Pools should be in a dict'
+assert type(constraints) == dict, 'Constraints should be in a dict'
+assert type(buyers) == list, 'Buyers should be in a list'
 
-    used_pool_ids = set([buyer.pool_id for buyer in buyers])
-    for pool_id in list(pools):
-        if pool_id not in used_pool_ids:
-            del pools[pool_id]
+# remove unnecessary loans (may be needed if this is a reduced input)
+used_loan_ids = set([buyer.loan_id for buyer in buyers])
+for loan_id in list(loans):
+    if loan_id not in used_loan_ids:
+        del loans[loan_id]
 
-    max_pool_id = max([pool_id for pool_id in pools])
-    PINGORA = max_pool_id + 1
-    TWO_HARBORS = max_pool_id + 2
-    ECONOMIC_VALUE = max_pool_id + 3
+used_pool_ids = set([buyer.pool_id for buyer in buyers])
+for pool_id in list(pools):
+    if pool_id not in used_pool_ids:
+        del pools[pool_id]
 
-    num_features = len(pools) * 2 + 5 + 5 + 1
+max_pool_id = max([pool_id for pool_id in pools])
+PINGORA = max_pool_id + 1
+TWO_HARBORS = max_pool_id + 2
+ECONOMIC_VALUE = max_pool_id + 3
 
-    config_values = np.zeros(shape=num_features)
-    config_normalizers = np.zeros(shape=num_features)
-    config_ids = sum([[(pool_id, 'c1'), (pool_id, 'c2')] for pool_id in pools], []) \
-        + [(PINGORA, 'c3'), (PINGORA, 'c4'), (PINGORA, 'c5'), (PINGORA, 'c6'), (PINGORA, 'c7')] \
-        + [(TWO_HARBORS, 'c8'), (TWO_HARBORS, 'c9'), (TWO_HARBORS, 'c10'), (TWO_HARBORS, 'c11'), (TWO_HARBORS, 'c12')] \
-        + [(ECONOMIC_VALUE, '')]
+config_ids = sum([[(pool_id, 'c1'), (pool_id, 'c2')] for pool_id in pools], []) \
+    + [(ECONOMIC_VALUE, '')]
+num_features = len(config_ids)
+config_values = np.zeros(shape=num_features)
+config_normalizers = np.ones(shape=num_features)
 
-    for i, config_id, normalizer, value in zip(range(num_features), config_ids, config_normalizers, config_values):
-        constraint_name = config_id[1]
-        if constraint_name:
-            config_values[i] = 0
-            config_normalizers[i] = 0
+for i, config_id in zip(range(num_features), config_ids):
+    constraint_name = config_id[1]
+    if constraint_name == 'c1':
+        config_normalizers[i] = 0
 
+logging.debug('Initial configuration vectors:')
+logging.debug(config_values)
+logging.debug(config_normalizers)
+logging.debug('--------------------------------------------------------\n')
+
+taken_buyers = []
+for a in range(len(buyers)):
+    # Choose some buyer
+    fixed_buyer = None
+    for buyer in buyers:
+        if buyer in taken_buyers: continue
+        fixed_buyer = buyer
+        break
+    if fixed_buyer  is None:
+        logging.error('All buyers taken!')
+        exit()
+    taken_buyers.append(fixed_buyer)
+
+    # Extract pool id and loan from that buyer
+    fixed_pool_id = fixed_buyer.pool_id
+    fixed_loan_id = fixed_buyer.loan_id
+    fixed_loan = loans[fixed_loan_id]
+
+    # Construct this buyer's translation vectors
+    move_values = np.zeros(shape=num_features)
+    move_normalizers = np.zeros(shape=num_features)
+
+    index_c1 = config_ids.index((fixed_pool_id, 'c1'))
+    move_values[index_c1] = fixed_loan.is_expensive
+    move_normalizers[index_c1] = fixed_loan.amount
+
+    index_c2 = index_c1 + 1
+    move_values[index_c2] = fixed_loan.amount
+
+    index_price = len(config_ids) - 1
+    move_values[index_price] = fixed_buyer.price
+
+    logging.debug(f'Translation vectors of iteration {a}:')
+    logging.debug(fixed_buyer)
+    logging.debug(move_values)
+    logging.debug(move_normalizers)
+    logging.debug('--------------------------------------------------------\n')
+
+    # Apply this buyer's translation vectors to the total configuration
+    config_values += move_values
+    config_normalizers += move_normalizers
+
+    logging.debug('Configuration vectors:')
     logging.debug(config_values)
-
-    fixed_loan_id = 103282778
-    loan = loans[fixed_loan_id]
-    loan
+    logging.debug(config_normalizers)
+    logging.debug('--------------------------------------------------------\n')
